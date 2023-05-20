f5fd37a67b3808d2c37b8dcda217da081520285e
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-05-19 19:55:59 -0600

    Setup project with the initial 16x16 grid

---
+++ myscripts.js
@@ -0,0 +1,12 @@
+let container = document.querySelector(".container");
+createGrid(16);
+
+function createGrid(size) {
+    for (let i=0; i<size**2; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.style.flexBasis = `${1/size*100}%`;
+        square.style.height = `${1/size*100}%`;
+        container.append(square);
+    }
+}
