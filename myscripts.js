efb2fad9fbe0b917920a94681cc8ff1d55ae5776
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-05-19 20:13:18 -0600

    Add a hovering effect that colors the squares

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,16 @@
 let container = document.querySelector(".container");
 createGrid(16);
 
+
 function createGrid(size) {
     for (let i=0; i<size**2; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         square.style.flexBasis = `${1/size*100}%`;
         square.style.height = `${1/size*100}%`;
+        square.addEventListener("mouseover", e => {
+            e.target.style.backgroundColor = "blue";
+        })
         container.append(square);
     }
 }
