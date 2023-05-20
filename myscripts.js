dbc9e8d77ec1c9361c544f3f5dbe96a04848cdc9
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-05-19 21:37:17 -0600

    Add Reset Grid functionality

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,8 @@
 let container = document.querySelector(".container");
 createGrid(16);
 
+let btn = document.querySelector("button");
+btn.addEventListener("click", resetGrid);
 
 function createGrid(size) {
     for (let i=0; i<size**2; i++) {
@@ -12,5 +14,13 @@ function createGrid(size) {
             e.target.style.backgroundColor = "blue";
         })
         container.append(square);
+    }
+}
+
+function resetGrid() {
+    let new_size = parseInt(prompt("How many squares do you want the grid to have at each side? Max 100 squares"));
+    if (typeof new_size === "number" && new_size > 1 && new_size <= 100) {
+        document.querySelectorAll(".square").forEach(square => square.remove());
+        createGrid(new_size);
     }
 }
